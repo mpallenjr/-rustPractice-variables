af4331eb5b4747be1a938590cf5d46a6aeb4ce3b
Mike Allen <mpallenjnr@gmail.com>
2023-02-28 15:21:37 -0600

    add a second function called by main()

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+
+// fn main() {
+//     // let mut x = 5;
+//     // println!("The value of x is: {x}");
+//     // x = 6;
+//     // println!("The value of x is: {x}");
+//     // println!("Time is {THREE_HOURS_IN_SECONDS}")
+
+//     let x = 5;
+
+//     let x = x + 1;
+
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
+
+//     println!("The value of x is: {x}");
+
+// }
 
 
 fn main() {
-    // let mut x = 5;
-    // println!("The value of x is: {x}");
-    // x = 6;
-    // println!("The value of x is: {x}");
-    // println!("Time is {THREE_HOURS_IN_SECONDS}")
+    println!("Hello, world!");
 
-    let x = 5;
+    another_function();
+}
 
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
-
+fn another_function() {
+    println!("Another function.");
 }
