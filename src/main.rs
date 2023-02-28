2c1e364bb7bb7c294122f7ded1bc28a85097016b
Mike Allen <mpallenjnr@gmail.com>
2023-02-28 14:40:17 -0600

    add global constant

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    println!("Time is {THREE_HOURS_IN_SECONDS}")
+
 }
